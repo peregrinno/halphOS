ffa67b0cf26633cd4e57f05d0ef2d9573f30eada
Luan Araújo <joseluan74@gmail.com>
2022-10-18 12:46:44 +0000

    refazendo...

--- MemoriaLRU.py
+++ MemoriaLRU.py
@@ -1,37 +1,20 @@
 import time, random
 
 
-def iniciaMemoria(HD, TAM_HD, TAM_memoriaFisica, TAM_memoriaVirtual, memoriaFisica, memoriaVirtual):
+def iniciaMemoria():
     print('Iniciando memória, aguarde...')
     #Tratemos o espaço como uma lista com dois valores [Valor, Tamanho]
     # EX: ESPACO = [0,1]
-    for i in range(TAM_HD):
-        VALOR = random.randint(1,9)
-        HD.append([i,VALOR])
+    
+     
 
-    for i in range(TAM_memoriaFisica):
-        memoriaFisica.append(['', 0])
-        memoriaVirtual.append('')
-        memoriaVirtual.append('')
-
-        
-    time.sleep(2)
-    print('Memória operacional.')
-
-def mostraMemoria(MEMORIA, memoriaFisica, memoriaVirtual):
-    for i in range(len(MEMORIA)):
-        print(f'Endereço: {i} Valor: {MEMORIA[i][1]}')
-    print(f'Memória Fisica: {memoriaFisica} \nMemória Virtual: {memoriaVirtual}')
+def mostraMemoria():
+   
 
 
-'''
-def acessoMemoriaLRU(tipo, end, valor):
-    if tipo == 'W':
-        if memoriaFisica[end] == '':
-            memoriaFisica[end] = valor
-        else: 
-       
-''' 
+def acessoMemoriaLRU():
+   
+
 
 
 
--- main.py
+++ main.py
@@ -2,12 +2,10 @@ from Gerente import Gerente
 import MemoriaLRU
 import time
 
-HD = []
-TAM_HD = 50
-memoriaFisica = [S]
-TAM_memoriaFisica = TAM_HD // 10
+
+
 memoriaVirtual = []
-TAM_memoriaVirtual = TAM_memoriaFisica * 2
+TAM_memoriaVirtual = []
 
 entrada = ['4-R', '5-R', '0-R','4-W-2', '1-R', '5-W-4', '2-R', '2-W-6']
 
@@ -15,7 +13,7 @@ print('Olá! Esse é o halphOS!')
 time.sleep(1)
 print('Vamos começar...')
 
-MemoriaLRU.iniciaMemoria(HD, TAM_HD, TAM_memoriaFisica, TAM_memoriaVirtual, memoriaFisica, memoriaVirtual)
+MemoriaLRU.iniciaMemoria()
 #MemoriaLRU.mostraMemoria(HD, memoriaFisica, memoriaVirtual)
 
 #gerente = Gerente(entrada)
